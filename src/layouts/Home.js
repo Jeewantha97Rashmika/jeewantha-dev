613fd4237f5e79b5f343d3e97b64c78be9d8e5e2
Jeewantha97Rashmika <jeewantharashmika3@gmail.com>
2025-01-12 21:45:04 +0530

    home bg color

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -6,33 +6,30 @@ import Projrcts from "./home/Projrcts";
 import FunActivity from "./home/FunActivity";
 import Testimonials from "./home/Testimonials";
 import Servicesnew from "./home/Servicesnew";
+import { Box, useTheme } from "@mui/material";
 
 export default function Home() {
+  const theme = useTheme();
   return (
     <>
-      <HeroSection />
-      {/* <SkillMarquee/> */}
-      {/* <TerminalLayout/> */}
-      <CountBanner />
-
-      {/* <Services /> */}
-      <Servicesnew />
-
-      <Projrcts />
-
-      <Testimonials />
-
-      <EduAndExp />
-
-      {/* <SkillAndTech/> */}
-
-      {/* <SkillGrid /> */}
-      {/* <BlogSection/> */}
-
-      <FunActivity />
-
-      {/* <ExploreWork /> */}
-      {/* <CursorOne /> */}
+      <Box sx={{ backgroundColor: theme.palette.bgColor?.main }}>
+        {" "}
+        <HeroSection />
+        {/* <SkillMarquee/> */}
+        {/* <TerminalLayout/> */}
+        <CountBanner />
+        {/* <Services /> */}
+        <Servicesnew />
+        <Projrcts />
+        <Testimonials />
+        <EduAndExp />
+        {/* <SkillAndTech/> */}
+        {/* <SkillGrid /> */}
+        {/* <BlogSection/> */}
+        <FunActivity />
+        {/* <ExploreWork /> */}
+        {/* <CursorOne /> */}
+      </Box>
     </>
   );
 }
