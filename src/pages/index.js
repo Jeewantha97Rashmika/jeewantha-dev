e9b414ad8adc480f0aa2933a7f123895fbf0cf26
Radika Dilanka <radika.insfra@gmail.com>
2025-03-16 00:15:11 +0530

    update seo title and desc and add og and canonical

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,70 @@
-import React, { useState, useEffect } from "react";
-import Header from "../layouts/Header";
-import Home from "../layouts/Home";
-import "../global.css";
-import Footer from "../layouts/Footer";
-import { Helmet } from "react-helmet";
-import { Box } from "@mui/material";
-import ThemeLayout from "../Theme/ThemeLayout";
-import ScrollToTop from "react-scroll-to-top";
+import React, { useState, useEffect } from 'react';
+import Header from '../layouts/Header';
+import Home from '../layouts/Home';
+import '../global.css';
+import Footer from '../layouts/Footer';
+import { Helmet } from 'react-helmet';
+import { Box } from '@mui/material';
+import ThemeLayout from '../Theme/ThemeLayout';
+import ScrollToTop from 'react-scroll-to-top';
 
 const IndexPage = () => {
   const [themeMode, setThemeMode] = useState(() => {
-    return typeof window !== "undefined"
-      ? localStorage.getItem("themeMode") || "light"
-      : "light";
+    return typeof window !== 'undefined'
+      ? localStorage.getItem('themeMode') || 'light'
+      : 'light';
   });
   // const theme = useTheme();
 
   useEffect(() => {
-    localStorage.setItem("themeMode", themeMode);
+    localStorage.setItem('themeMode', themeMode);
   }, [themeMode]);
 
   const toggleTheme = () => {
-    setThemeMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+    setThemeMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
   };
 
   return (
     <>
       <Helmet>
-        <title>Jeewantha Rashmika | UI/UX Designer</title>
+        <title>Jeewantha Rashmika - UI/UX Designer & Engineer Sri Lanka</title>
 
         <meta
-          name="description"
-          content="UI/UX designer who is interested in solving complex problems through designing and mobile app/web development"
+          name='description'
+          content=' UI/UX designer & engineer specializing in user-friendly web & app design, prototyping, and development. Creating seamless digital experiences.'
+        />
+        <link rel='canonical' href='https://jeewantharashmika.com/' />
+        <meta
+          property='og:title'
+          content='UI/UX Designer & Engineer | Expert in Web & App Design'
+        />
+        <meta
+          property='og:description'
+          content='UI/UX designer & engineer specializing in user-friendly web & app design, prototyping, and development. Creating seamless digital experiences.'
+        />
+        <meta property='og:url' content='https://jeewantharashmika.com/' />
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:image'
+          content='https://yourwebsite.com/og-image.jpg'
+        />
+        <meta
+          property='og:site_name'
+          content='Jeewantha - UI/UX Designer & Engineer'
         />
       </Helmet>
 
       <ThemeLayout themeMode={themeMode}>
         <Box>
           <Header themeMode={themeMode} toggleTheme={toggleTheme} />
-          <ScrollToTop top={900} smooth color="#333" style={{
-            zIndex: 100000,
-         
-          }} />
+          <ScrollToTop
+            top={900}
+            smooth
+            color='#333'
+            style={{
+              zIndex: 100000,
+            }}
+          />
           <Home />
 
           <Footer />
