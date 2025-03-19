b3fc92d214b0f219b0a93ac060db6d44af7318d6
Radika Dilanka <radika.insfra@gmail.com>
2025-03-19 11:55:47 +0530

    add theme mode custom hook

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,54 +1,42 @@
-import React, { useState, useEffect } from "react";
-import Header from "../../layouts/Header";
+import React from 'react';
+import Header from '../../layouts/Header';
 
-import "../../global.css";
-import ProjectGrid from "../../layouts/home/ProjectGrid";
-import SeoSection from "../../components/shoezone/SeoSection";
-import ThemeLayout from "../../Theme/ThemeLayout";
-import { Box } from "@mui/material";
-import ScrollToTop from "../../components/ScrollToTop";
-import Footer from "../../layouts/Footer";
+import '../../global.css';
+import ProjectGrid from '../../layouts/home/ProjectGrid';
+import SeoSection from '../../components/shoezone/SeoSection';
+import ThemeLayout from '../../Theme/ThemeLayout';
+import { Box } from '@mui/material';
+import ScrollToTop from '../../components/ScrollToTop';
+import Footer from '../../layouts/Footer';
+import useThemeMode from '../../customHooks/useThemeMode';
 
 export default function Index() {
-  const [themeMode, setThemeMode] = useState(() => {
-    return typeof window !== "undefined"
-      ? localStorage.getItem("themeMode") || "light"
-      : "light";
-  });
-  // const theme = useTheme();
-
-  useEffect(() => {
-    localStorage.setItem("themeMode", themeMode);
-  }, [themeMode]);
-
-  const toggleTheme = () => {
-    setThemeMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-  };
+  const { themeMode, toggleTheme } = useThemeMode();
 
   return (
     <div>
       <SeoSection
-        title={"Photographer Portfolio Website Design"}
+        title={'Photographer Portfolio Website Design'}
         description={
           "Elegant UI design for a professional photographer's digital platform, enhancing brand identity while showcasing exceptional photography."
         }
-        canonical={"projects/"}
+        canonical={'projects/'}
       />
 
       <ThemeLayout themeMode={themeMode}>
         <Box
           sx={{
-            backgroundColor: themeMode === "light" ? "#f7f8fa" : "#0b0b0d",
-            transition: "background-color 0.3s ease, color 0.3s ease",
+            backgroundColor: themeMode === 'light' ? '#f7f8fa' : '#0b0b0d',
+            transition: 'background-color 0.3s ease, color 0.3s ease',
           }}
         >
           <Header themeMode={themeMode} toggleTheme={toggleTheme} />
           <ScrollToTop
             top={900}
             smooth
-            color="#333"
+            color='#333'
             style={{
-              padding: "5px",
+              padding: '5px',
               zIndex: 100000,
             }}
           />
--- src/pages/projects/x-camera/index.js
+++ src/pages/projects/x-camera/index.js
@@ -1,47 +1,33 @@
-import React, { useState, useEffect } from "react";
-import Header from "../../../layouts/Header";
-import ScrollToTop from "react-scroll-to-top";
-import "../../../global.css";
-import Footer from "../../../layouts/Footer";
-import { Box } from "@mui/material";
-import ThemeLayout from "../../../Theme/ThemeLayout";
-import Layout from "../../../layouts/xCamera/Layout";
-import SeoSection from "../../../components/shoezone/SeoSection";
-import DesignImage from "../../../images/photography-portfolio.png";
+import React from 'react';
+import Header from '../../../layouts/Header';
+import ScrollToTop from 'react-scroll-to-top';
+import '../../../global.css';
+import Footer from '../../../layouts/Footer';
+import { Box } from '@mui/material';
+import ThemeLayout from '../../../Theme/ThemeLayout';
+import Layout from '../../../layouts/xCamera/Layout';
+import SeoSection from '../../../components/shoezone/SeoSection';
+import DesignImage from '../../../images/photography-portfolio.png';
+import useThemeMode from '../../../customHooks/useThemeMode';
 
 const IndexPage = () => {
-  const [themeMode, setThemeMode] = useState(() => {
-    return typeof window !== "undefined"
-      ? localStorage.getItem("themeMode") || "light"
-      : "light";
-  });
-  // const theme = useTheme();
-
-  useEffect(() => {
-    localStorage.setItem("themeMode", themeMode);
-  }, [themeMode]);
-
-
-
-  const toggleTheme = () => {
-    setThemeMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-  };
+  const { themeMode, toggleTheme } = useThemeMode();
 
   return (
     <>
       <SeoSection
-        title={"Photographer Portfolio Website Design"}
+        title={'Photographer Portfolio Website Design'}
         description={
           "Elegant UI design for a professional photographer's digital platform, enhancing brand identity while showcasing exceptional photography."
         }
-        canonical={"projects/x-camera/"}
+        canonical={'projects/x-camera/'}
       />
 
       <ThemeLayout themeMode={themeMode}>
         <Box
           sx={{
-            backgroundColor: themeMode === "light" ? "#f7f8fa" : "#0b0b0d",
-            transition: "background-color 0.3s ease, color 0.3s ease",
+            backgroundColor: themeMode === 'light' ? '#f7f8fa' : '#0b0b0d',
+            transition: 'background-color 0.3s ease, color 0.3s ease',
           }}
         >
           <Header themeMode={themeMode} toggleTheme={toggleTheme} />
@@ -54,16 +40,16 @@ const IndexPage = () => {
               zIndex: 100000,
             }}
           />
-      
+
           <Layout
             cover={
-              "https://cdn.jeewantharashmika.com/projects/x_camera_cover.png"
+              'https://cdn.jeewantharashmika.com/projects/x_camera_cover.png'
             }
             projectImage={DesignImage}
-            title="Photographer Portfolio Website Design"
-            roles={"UI/UX Designer"}
+            title='Photographer Portfolio Website Design'
+            roles={'UI/UX Designer'}
             description="My recent UI design project showcases a sophisticated digital platform for a professional photographer, creating a visual narrative that elevates their brand while highlighting their exceptional photography skills. The interface serves as an elegant canvas that complements the photographer's work without overshadowing it."
-            client="Personal Project"
+            client='Personal Project'
           />
           <Footer />
         </Box>
